9b48c96e939c1d48a49261491e28c181d5278acd
Ruchika Sreedhar <47388335+RuchikaS2912@users.noreply.github.com>
2020-05-20 11:19:17 +0530

    Update MainPart.js

--- frontend/src/components/MainPart.js
+++ frontend/src/components/MainPart.js
@@ -1,10 +1,37 @@
-import React from 'react';
+import React,{Component} from 'react';
 import {Form,Col} from 'react-bootstrap';
 // import oil from '../images/oil.jpg';
 import './style.css';
 import ProfileIcon from './ProfileIcon';
 
-export default function Formfield() {
+export default class Formfield extends Component {
+    constructor () {
+    super();
+    this.state = {
+        firstname:'',
+        lastname:'',
+        telephone:'',
+        email: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.onHandleTelephoneChange = this.onHandleTelephoneChange.bind(this);
+  }
+
+  handleChange (evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+    
+    onHandleTelephoneChange = e => {
+        let telephone = e.target.value;
+
+        if (!Number(telephone)) {
+            return;
+        }
+        this.setState({
+            [e.target.name]: telephone
+        });
+    };
+    
     return (
         <div className="formfield-maindiv" style={{paddingTop:'30px',display:'inline-block'}}>
             <div className="first">
@@ -16,23 +43,23 @@ export default function Formfield() {
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control type="text" placeholder="First name" />
+                    <Form.Control type="text" name="firstname" placeholder="First name" onChange={this.handleOnChange} />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control type="text" placeholder="Last Name"/>
+                    <Form.Control type="text" name="lastname" placeholder="Last Name" onChange={this.handleOnChange}/>
                     </Form.Group>
                 </Form.Row>
                 <Form.Row>
                     <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>Mobile Number</Form.Label>
-                    <Form.Control type="tel" placeholder="Mobile Number" />
+                    <Form.Control type="tel" name="telephone" placeholder="Mobile Number"  onChange = {this.onHandleTelephoneChange} />
                     </Form.Group>
 
                     <Form.Group as={Col} controlId="formGridCity">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="E-mail"/>
+                    <Form.Control type="email" name="email" placeholder="E-mail" onChange={this.handleOnChange}/>
                     </Form.Group>
                 </Form.Row>
                 </div>
